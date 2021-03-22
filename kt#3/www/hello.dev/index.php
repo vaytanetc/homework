2fbb469f9b9171d5e99074318e3b5b3cdef06893
vaytanetc <63734694+vaytanetc@users.noreply.github.com>
2021-03-22 20:39:10 +0500

    Create index.php

---
+++ kt#3/www/hello.dev/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+<body>
+<p>В лофте n хипстерам достались m смузи. При этом все хипстеры – люди вежливые, и поэтому должны выпить одинаковое количество смузи (можно выбросить несколько). Напишите функцию, результатом которой будет количество смузи, которое выпьет каждый хипстер. </p>
+<p>Введите количество хипстеров </p>
+<input name="hip" type="text">
+<p>Введите количество смузи </p>
+<input name="smuzi" type="text">
+<button id="button">Узнать количество смузи</button>
+<p class="out"></p>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
+    $(document).on('click', '#button', function(){
+        var $data = {};
+        $(document).find ('input, textearea, select').each(function() {
+            $data[this.name] = $(this).val();});
+        $.ajax({
+            url: 'func.php',
+            type: 'GET',
+            data: $data,
+            success: function(data){
+                $('p.out').text(data);},
+            error: function(){
+                console.log('ERROR');}})})
+</script>
+</body>
+</html>
+
+
+
+
+
