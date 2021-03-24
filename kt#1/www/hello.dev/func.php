f13124c56a428ee44a51e3ba3c1072f7cfd9a714
vaytanetc <63734694+vaytanetc@users.noreply.github.com>
2021-03-24 23:59:08 +0500

    Update func.php

--- kt#1/www/hello.dev/func.php
+++ kt#1/www/hello.dev/func.php
@@ -1,13 +1,10 @@
 <?php
 $year=$_GET['Year'];
-$text="";
 if (is_numeric($year)) {
     if ($year % 400 == 0 or $year % 100 == 0 or $year % 4 == 0) {
-        $text = " ДА";}
+        echo " ДА";}
     else {
-        $text = " НЕТ";}}
-else {
-    $text = " ОШИБКА ВО ВХОДНЫХ ДАННЫХ";}
-echo $year;
-echo "$text";
+        echo " НЕТ";}}
+else { 
+    echo " ОШИБКА ВО ВХОДНЫХ ДАННЫХ";}
 ?> 
