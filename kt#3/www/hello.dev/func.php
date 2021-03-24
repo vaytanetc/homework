ec38a541143130969e2550293f9fc3e93bee2558
vaytanetc <63734694+vaytanetc@users.noreply.github.com>
2021-03-24 23:16:54 +0500

    Update func.php

--- kt#3/www/hello.dev/func.php
+++ kt#3/www/hello.dev/func.php
@@ -1,8 +1,10 @@
 <?php
-$hip=$_GET['hip'];
-$smuzi=$_GET['smuzi'];
-if (is_numeric($smuzi) and is_numeric($hip)) {
-    $kol=(int)($smuzi / $hip);
-    echo "Количество смузи =  $kol";}
-else { echo "Неверные входные данные";}
-?> 
+$hip = $_GET['hip'];
+$smuzi = $_GET['smuzi'];
+function f ($hip, $smuzi){
+    if (is_numeric($smuzi) and is_numeric($hip)) {
+        $kol = (int)($smuzi / $hip);
+        echo "Количество смузи =  $kol";} 
+    else {
+        echo "Неверные входные данные";}}
+$kol=f($hip, $smuzi);
